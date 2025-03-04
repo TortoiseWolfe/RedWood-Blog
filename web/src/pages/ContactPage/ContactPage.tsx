442815f451aaa9875d10d0c2ba007ff28346ae1f
TurtlWolfe <you@example.com>
2025-03-04 12:35:57 -0500

    Chapter 3: Saving Data

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,13 +1,29 @@
 import {
+  CreateContactMutation,
+  CreateContactMutationVariables,
+} from 'types/graphql'
+
+import {
   FieldError,
   Form,
+  FormError,
   Label,
   TextField,
   TextAreaField,
   Submit,
   SubmitHandler,
+  useForm,
 } from '@redwoodjs/forms'
-import { Metadata } from '@redwoodjs/web'
+import { Metadata, useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 interface FormValues {
   name: string
@@ -16,15 +32,35 @@ interface FormValues {
 }
 
 const ContactPage = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+  const [create, { loading, error }] = useMutation<
+    CreateContactMutation,
+    CreateContactMutationVariables
+  >(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission!')
+      formMethods.reset()
+    },
+  })
+
   const onSubmit: SubmitHandler<FormValues> = (data) => {
+    create({ variables: { input: data } })
     console.log(data)
   }
 
   return (
     <>
       <Metadata title="Contact" description="Contact page" />
+      <Toaster />
 
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        {/* <FormError error={error} wrapperClassName="form-error" /> */}
+
         <Label name="name" errorClassName="error">
           Name
         </Label>
@@ -34,6 +70,7 @@ const ContactPage = () => {
           errorClassName="error"
         />
         <FieldError name="name" className="error" />
+
         <Label name="email" errorClassName="error">
           Email
         </Label>
@@ -49,6 +86,7 @@ const ContactPage = () => {
           errorClassName="error"
         />
         <FieldError name="email" className="error" />
+
         <Label name="message" errorClassName="error">
           Message
         </Label>
@@ -58,7 +96,8 @@ const ContactPage = () => {
           errorClassName="error"
         />
         <FieldError name="message" className="error" />
-        <Submit>Save</Submit>
+
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </>
   )
